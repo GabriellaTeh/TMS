247d641e9684cd2d8c87015e7c80ae9ab22c7729
GabriellaTeh <gabriella.teh@u.nus.edu>
2024-01-30 15:42:16 +0800

    create new header for admin homepage

---
+++ client/app/components/HeaderHomeAdmin.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import DispatchContext from "../DispatchContext";
+
+function HeaderHomeAdmin() {
+  const appDispatch = useContext(DispatchContext);
+  const navigate = useNavigate();
+
+  function handleUserManagement() {}
+
+  function handleLogout() {
+    appDispatch({ type: "logout" });
+    navigate("/");
+  }
+  function handleEditProfile() {
+    appDispatch({ type: "edit" });
+    navigate("/profile");
+  }
+
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <button
+        onClick={handleUserManagement}
+        className="btn btn-sm btn-secondary"
+      >
+        User Management
+      </button>{" "}
+      <button onClick={handleEditProfile} className="btn btn-sm btn-secondary">
+        Edit Profile
+      </button>{" "}
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  );
+}
+
+export default HeaderHomeAdmin;
