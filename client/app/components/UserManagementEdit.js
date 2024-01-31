38fb1ba329c0d5e1442a4b779eb7db623615f527
GabriellaTeh <gabriella.teh@u.nus.edu>
2024-01-31 17:35:44 +0800

    add table

--- client/app/components/UserManagementEdit.js
+++ client/app/components/UserManagementEdit.js
@@ -20,6 +20,29 @@ function UserManagementEdit() {
           Quit without saving
         </button>
       </div>
+      <div className="mt-3">
+        <table id="userTable" className="table table-hover">
+          <thead>
+            <tr>
+              <th className="col-2" scope="col">
+                Username
+              </th>
+              <th className="col-2" scope="col" data-editable="true">
+                Password
+              </th>
+              <th className="col-2" scope="col">
+                Email
+              </th>
+              <th className="col-auto" scope="col">
+                Groups
+              </th>
+              <th className="col-sm-2" scope="col">
+                Active
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </>
   );
 }
--- client/app/components/UserManagementView.js
+++ client/app/components/UserManagementView.js
@@ -1,8 +1,22 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import DispatchContext from "../DispatchContext";
 
 function UserManagementView() {
   const appDispatch = useContext(DispatchContext);
+  const [users, setUsers] = useState([]);
+
+  async function getUsers() {
+    try {
+      const response = await Axios.get("/users");
+
+      if (response.data) {
+        setUsers(response.data);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   function handleEdit() {
     appDispatch({ type: "editing" });
   }
@@ -12,6 +26,29 @@ function UserManagementView() {
         <button onClick={handleEdit} className="btn btn-primary btn-sm">
           Edit Mode
         </button>
+      </div>
+      <div className="mt-3">
+        <table id="userTable" className="table table-hover">
+          <thead>
+            <tr>
+              <th className="col-2" scope="col">
+                Username
+              </th>
+              <th className="col-2" scope="col" data-editable="true">
+                Password
+              </th>
+              <th className="col-2" scope="col">
+                Email
+              </th>
+              <th className="col-auto" scope="col">
+                Groups
+              </th>
+              <th className="col-sm-2" scope="col">
+                Active
+              </th>
+            </tr>
+          </thead>
+        </table>
       </div>
     </>
   );
