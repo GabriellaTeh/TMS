f1e64809cd7e7d12a964b9e25d03a5bfb8528447
GabriellaTeh <gabriella.teh@u.nus.edu>
2024-02-08 17:37:39 +0800

    shift header components to the edge

--- client/app/components/Header.js
+++ client/app/components/Header.js
@@ -31,7 +31,7 @@ function Header() {
   return (
     <>
       <header className="header-bar bg-primary mb-3">
-        <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+        <div className="d-flex flex-column flex-md-row align-items-center p-3">
           <h4 className="my-0 mr-md-auto font-weight-normal">
             <a href="" className="text-white">
               TMS
