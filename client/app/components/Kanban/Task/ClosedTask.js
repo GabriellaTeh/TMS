fd3be69efa10b69f17acd6b187b7251ab02a16c5
GabriellaTeh <gabriella.teh@u.nus.edu>
2024-02-27 16:23:04 +0800

    rearrange closed task

--- client/app/components/Kanban/Task/ClosedTask.js
+++ client/app/components/Kanban/Task/ClosedTask.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
 import { TextField, Autocomplete, Button } from "@mui/material";
+import dayjs from "dayjs";
 
 function ClosedTask(props) {
   const navigate = useNavigate();
@@ -16,20 +17,13 @@ function ClosedTask(props) {
     <>
       <Grid container spacing={3} className="mt-1">
         <Grid item xs={6}>
-          <div className="form-group">
-            <label className="text-muted mb-1">
-              <small>Task Description</small>
-            </label>
-            <TextField
-              fullWidth
-              InputProps={{ readOnly: true }}
-              multiline
-              rows={7}
-              defaultValue={props.description}
-            ></TextField>
-          </div>
-        </Grid>
-        <Grid item xs={6}>
+          <h4>
+            Task #{task}: {props.taskName}
+          </h4>
+          Created by: {props.creator} <br></br> Created on:{" "}
+          {dayjs(props.createDate).format("DD-MM-YYYY")}
+          <br></br>Owner: {props.owner}
+          <br></br> State: closed
           <div className="form-group">
             <label className="text-muted mb-1">
               <small>Plan Name</small>
@@ -44,6 +38,20 @@ function ClosedTask(props) {
               )}
             />
           </div>
+          <div className="form-group">
+            <label className="text-muted mb-1">
+              <small>Task Description</small>
+            </label>
+            <TextField
+              fullWidth
+              InputProps={{ readOnly: true }}
+              multiline
+              rows={7}
+              defaultValue={props.description}
+            ></TextField>
+          </div>
+        </Grid>
+        <Grid item xs={6}>
           <div className="form-group">
             <label className="text-muted mb-1">
               <small>Task Notes</small>
